759b76f2228c948bdd741d9aadaa44e34309472e
Soluções Software <ssolucoessoftware@gmail.com>
2024-02-27 12:27:18 -0300

    ➕new: Dashboard Auto escola
    inserido fullcalendar vue com alguns plugin iniciais

---
+++ resources/js/Components/Card/CardCount.vue
@@ -0,0 +1,7 @@
+<template>
+    Crad count here
+</template>
+<script setup>
+</script>
+<style scoped lang="scss">
+</style>
--- resources/js/Pages/DrivingSchool/Dashboard.vue
+++ resources/js/Pages/DrivingSchool/Dashboard.vue
@@ -1,15 +1,43 @@
 <template>
     <AppLayout title="Dashboard" title_page="Dashboard - Auto Escola">
-        <Fieldset legend="Sistemas">
-            Auto escola aq
-        </Fieldset>
+        <div>
+            <Fieldset legend="Agendamentos">
+                <FullCalendar :options="calendarOptions" />
+            </Fieldset>
+        </div>
+        <div class="mt-5">
+            <Fieldset legend="Sua rede">
+                <CardCount></CardCount>
+            </Fieldset>
+        </div>
     </AppLayout>
 </template>
 <script setup>
+import { ref } from 'vue';
 import { usePage } from '@inertiajs/vue3';
-//components
 
+//components
+import CardCount from '@/Components/Card/CardCount.vue';
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction';
+import ptbrLocale from '@fullcalendar/core/locales/pt-br';
 //fim components
+
+const calendarOptions = ref({
+    plugins: [dayGridPlugin,timeGridPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    height: 650,
+    locales: [ptbrLocale],
+    locale: 'pt-br',
+    headerToolbar: {
+        left: 'prev,next,today',
+        center: 'title',
+        // right: 'timeGridDay,timeGridWeek,dayGridMonth,dayGridYear'
+        right: 'dayGridDay,timeGridDay,timeGridWeek,dayGridWeek,dayGridMonth,dayGridYear' // user can switch between the two
+    }
+});
 
 const page = usePage();
 </script>
