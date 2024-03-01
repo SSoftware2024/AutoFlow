d3de2c96842451ffb7a785ebb86debf2cf0a3853
Soluções Software <ssolucoessoftware@gmail.com>
2024-03-01 10:46:28 -0300

    ✨ new: Cadastro de veículo
    foi ajustadoa migration retirada coluna ttype e deixada categoria como enum

--- app/Http/Controllers/DrivingSchool/VehiclesController.php
+++ app/Http/Controllers/DrivingSchool/VehiclesController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\DrivingSchool;
 
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use App\Facade\MessagesFactory;
 use App\Facade\NavigateFactory;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\MessageBag;
+use App\Enum\Database\DrivingWallet;
 use App\Http\Controllers\Controller;
+use App\Models\DrivingSchool\Vehicles;
+use Illuminate\Support\Facades\Auth;
 
 class VehiclesController extends Controller
 {
@@ -19,12 +26,46 @@ class VehiclesController extends Controller
             ->setLink('Novo');
         return Inertia::render('DrivingSchool/Vehicles/Create', [
             'breadcrumb' => $breadcrumb->generate(),
+            'driving_wallet' => DrivingWallet::getArrayObjectSelect(),
         ]);
     }
     /**===================================METODOS ROUTES=================================== */
     public function create(Request $request)
     {
-        ds($request->all(), strtoupper($request->plate));
+        $request->validate([
+            'surname' => ['required','min:5'],
+            'plate' => ['required'],
+            'category' => ['required', Rule::enum(DrivingWallet::class)],
+            'ipva_generate' => ['required', 'date'],
+            'ipva_value' => ['required', 'numeric'],
+        ],[],[
+            'surname' => 'modelo carro',
+            'plate' => 'placa',
+            'ipva_generate' => 'IPVA data',
+            'ipva_value' => 'IPVA valor',
+        ]);
+        try {
+            DB::transaction(function () use ($request) {
+                $data = $request->all();
+                $data['plate'] = strtoupper($data['plate']);
+                $user = Auth::user();
+                Vehicles::create([
+                    ...$data,
+                    'company_id' => $user->company_id,
+                    'user_id' => $user->id,
+                ]);
+                MessagesFactory::toast()->success('Operação concluída com sucesso')
+                ->generate();
+            });
+        } catch (\Exception $e) {
+            $errors = new MessageBag();
+            $errors->add('catch', $e->getMessage());
+            return redirect()->back()->withErrors($errors);
+        }catch (\Error $e) {
+            $errors = new MessageBag();
+            $errors->add('catch', $e->getMessage());
+            return redirect()->back()->withErrors($errors);
+        }
     }
     /**===================================METODOS ROUTES AXIOS=================================== */
     /**===================================METODOS VARIADOS=================================== */
--- resources/js/ComponentsPage/DrivingSchool/Vehicles/Create.vue
+++ resources/js/ComponentsPage/DrivingSchool/Vehicles/Create.vue
@@ -3,24 +3,29 @@
         <div class="row sm:space-x-1">
             <div class="row-col">
                 <label>Modelo Carro</label>
-                <InputText v-model="form.name" id="username" :class="{
-                    'p-invalid': form.errors.name
+                <InputText v-model="form.surname" id="username" :class="{
+                    'p-invalid': form.errors.surname
                 }" />
+                <span class="text-danger-500" v-if="form.errors.surname">{{ form.errors.surname }}</span>
             </div>
             <div class="row-col">
                 <label>Categoria</label>
                 <div class="card flex justify-center">
-                    <Dropdown v-model="form.driving_wallet" display="chip" :options="driving_wallet" optionLabel="name"
-                        optionValue="code" placeholder="Selecione" class="w-full md:w-20rem" />
+                    <Dropdown v-model="form.category" display="chip" :options="$page.props.driving_wallet"
+                        optionLabel="name" optionValue="code" placeholder="Selecione" class="w-full md:w-20rem" :class="{
+                            'p-invalid': form.errors.category
+                        }" />
                 </div>
+                <span class="text-danger-500" v-if="form.errors.category">{{ form.errors.category }}</span>
             </div>
         </div>
         <div class="row sm:space-x-1">
             <div class="row-col">
                 <label>Placa</label>
                 <InputMask id="cnpj" v-model="form.plate" :mask="changeMask" :placeholder="changeMask" :class="{
-                    'p-invalid': false
+                    'p-invalid': form.errors.plate
                 }" :pt="{ root: 'uppercase' }" />
+                <span class="text-danger-500" v-if="form.errors.plate">{{ form.errors.plate }}</span>
             </div>
             <div class="row-col">
                 <label>Modelo Placa</label>
@@ -33,7 +38,10 @@
         <div class="row sm:space-x-1">
             <div class="row-col">
                 <label>IPVA data</label>
-                <Calendar v-model="form.ipva_date" dateFormat="dd/mm" showIcon iconDisplay="input" />
+                <Calendar v-model="form.ipva_generate" dateFormat="dd/mm" showIcon iconDisplay="input" :class="{
+                    'p-invalid': form.errors.ipva_generate
+                }" />
+                <span class="text-danger-500" v-if="form.errors.ipva_generate">{{ form.errors.ipva_generate }}</span>
             </div>
             <div class="row-col">
                 <label>IPVA valor</label>
@@ -41,6 +49,7 @@
                     :maxFractionDigits="2" mode="currency" currency="BRL" :class="{
                         'p-invalid': form.errors.ipva_value
                     }" />
+                <span class="text-danger-500" v-if="form.errors.ipva_value">{{ form.errors.ipva_value }}</span>
             </div>
         </div>
         <div class="justify-end mt-2 row">
@@ -54,22 +63,14 @@ import { ref, computed } from "vue";
 import { useForm } from '@inertiajs/vue3';
 
 const form = useForm({
-    name: '',
+    surname: '',
     plate: '',
-    driving_wallet: '',
+    category: '',
     ipva_value: null,
-    ipva_date: null
+    ipva_generate: null //data ipva
 });
 
 const model_plate = ref(0);
-const driving_wallet = ref([
-    { name: 'ACC', code: 'ACC' },
-    { name: 'A', code: 'A' },
-    { name: 'B', code: 'B' },
-    { name: 'C', code: 'C' },
-    { name: 'D', code: 'D' },
-    { name: 'E', code: 'E' },
-]);
 const model_plate_list = ref([
     { name: 'ANTIGO - 1990: AAA-9999', code: 0 },
     { name: 'NOVO - 2018: AAA9A99', code: 1 },
@@ -88,6 +89,10 @@ const changeMask = computed({
 
 
 function create() {
-    form.post(route('user.driving_school.vehicles.create'));
+    form.post(route('user.driving_school.vehicles.create'), {
+        onSuccess: () => {
+            form.reset();
+        }
+    });
 }
 </script>
